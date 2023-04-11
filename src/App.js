d53e604e0f3f450545375ebb644c3951784bd8ae
obinna <biostechnologyng@gmail.com>
2023-04-11 09:41:44 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,47 +1,86 @@
-import { useState } from "react";
-function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(email);
-    console.log(password);
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  const fetchUserData = () => {
+    fetch("http://localhost:3000/api/users", { method: "GET" })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => setUsers(data.users))
+      .catch((error) => console.log(error));
+  };
+
+  const fetchPosts = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts", { method: "GET" })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setPosts(data);
+      })
+      .catch((error) => console.log(error));
   };
   return (
     <div className="container">
-      <h1 className="display-4 text-center">Learn react</h1>
-      <br />
+      {users && users.map((user) => <div key={user.age}>{user.name}</div>)}
+      <buttton className="btn btn-primary" onClick={fetchUserData}>
+        fetch user data
+      </buttton>
 
-      <div className="row">
-        <div className="col-md-6 offset-md-3">
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label className="form-label">Email address</label>
-              <input
-                value={email}
-                type="email"
-                onChange={(event) => setEmail(event.target.value)}
-                className="form-control"
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Password</label>
-              <input
-                value={password}
-                type="password"
-                onChange={(event) => setPassword(event.target.value)}
-                className="form-control"
-              />
-            </div>
-
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </form>
-        </div>
-      </div>
+      {posts && posts.map((post) => <div key={post.id}>{post.title}</div>)}
     </div>
   );
-}
+};
+
+// function App() {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     console.log(email);
+//     console.log(password);
+//   };
+//   return (
+//     <div className="container">
+//       <h1 className="display-4 text-center">Learn react</h1>
+//       <br />
+
+//       <div className="row">
+//         <div className="col-md-6 offset-md-3">
+//           <form onSubmit={handleSubmit}>
+//             <div className="mb-3">
+//               <label className="form-label">Email address</label>
+//               <input
+//                 value={email}
+//                 type="email"
+//                 onChange={(event) => setEmail(event.target.value)}
+//                 className="form-control"
+//               />
+//             </div>
+//             <div className="mb-3">
+//               <label className="form-label">Password</label>
+//               <input
+//                 value={password}
+//                 type="password"
+//                 onChange={(event) => setPassword(event.target.value)}
+//                 className="form-control"
+//               />
+//             </div>
+
+//             <button type="submit" className="btn btn-primary">
+//               Submit
+//             </button>
+//           </form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default App;
